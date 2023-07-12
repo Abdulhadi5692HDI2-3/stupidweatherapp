75d8c6dde8da4835bdc0fd5670f96a464790edf6
Abdulhadi5692HDI2 <abdulhadi5692hdi2@gmail.com>
2023-07-12 20:09:25 +0100

    Add more stupid changes

--- src/index.php
+++ src/index.php
@@ -7,6 +7,7 @@
     <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link href="mphase.css" rel="stylesheet">
+    <link href="index.css" rel="stylesheet">
 </head>
 <body>
         <!-- Nav tabs -->
@@ -27,17 +28,48 @@
                 <h1>Weather</h1>
                 <?php
                     $variablea = $_GET["region"];
+                    $conta = file_get_contents("https://wttr.in/?format=%f");
+                    $conta2 = "↑" . $conta[1] . $conta[2] . $conta[3] . $conta[4] . $conta[5];
                     /*$bildstr = "https://wttr.in/" . $variablea . "?format=j1";*/
                     $obj = json_decode(file_get_contents("https://wttr.in/?format=j1"), true);
                     echo "<h2>", get_weather_icon(), $obj['current_condition'][0]['weatherDesc'][0]['value'], "</h2><br>";
-                    echo "Temperature: <b><i>", $obj['current_condition'][0]['temp_C'], "°C</i></b> <br>";
+                    echo "Temperature: <b><i>", $obj['current_condition'][0]['temp_C'], "°C</i></b>  <span style='color: #00ff00;'>", $conta2, "</span><br>";
                     echo "Feels like: <b><i>", $obj['current_condition'][0]['FeelsLikeC'], "°C</i></b> <br>";
-                    echo "Maximum Temperature to reach: <b><i>", $obj['weather'][0]['maxtempC'], "°C</i></b> <br>";
+
                     function get_weather_icon() {
                         return file_get_contents("https://wttr.in/?format=%c");
                     }
                     ?>
-                <h3>Weather Report</h3>
+                <div class="container-fluid col rounded-1 border-2" style="padding: 50px;width:250px;background-image: url('https://abdulhadi5692hdi2.github.io/index.jpg');">
+                    <div class="row-sm-3 wht">Sunrise: <b><i><?php 
+                        echo $obj['weather'][0]['astronomy'][0]['sunrise'];
+                    ?></i></b>
+                    </div>
+                    <div class="row-sm-3 wht">Sunset: <b><i><?php 
+                        echo $obj['weather'][0]['astronomy'][0]['sunset'];
+                    ?></i></b>
+                    </div>
+                    <div class="row-sm-3 wht">Moonrise: <b><i><?php 
+                        echo $obj['weather'][0]['astronomy'][0]['moonrise'];
+                    ?></i></b>
+                    </div>
+                    <div class="row-sm-3 wht">Moonset: <b><i><?php 
+                        echo $obj['weather'][0]['astronomy'][0]['moonset'];
+                    ?></i></b>
+                    </div>
+                </div>
+                <h3>Weather Report</h3> <button type="button" onclick="changeaccordingly();" id="ident" class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#w++">Show More ↓</button>
+                <script>
+                    function changeaccordingly() {
+                        var a = document.getElementById("ident");
+                        if (a.innerHTML == "Show Less ↑") {
+                            a.innerHTML="Show More ↓";
+                            return;
+                        }
+                        a.innerHTML="Show Less ↑";
+                    }
+                </script>
+                <div class="collapse" id="w++">
                 <?php
                 $variablea = $_GET["region"];
                 if ($variablea == NULL) {
@@ -51,9 +83,9 @@
                 ?>
                 <h3>Stuff to do with wind</h3>
                 <?php
-                    echo "Wind Speed: <b><i>", $obj['current_condition'][0]['windspeedKmph'], " kmph</i></b><br>";
+                    echo "Wind Speed: <b><i>→", $obj['current_condition'][0]['windspeedKmph'], " km/h</i></b><br>";
                 ?>
-                <h3>Moon Phase: <mphase><?php echo(file_get_contents("https://wttr.in/?format=%m"));?></mphase>(may display incorrectly)</h3>
+                <h3>Moon Phase: <mphase><?php echo(file_get_contents("https://wttr.in/?format=%m"));?></mphase>(<?php echo $obj['weather'][0]['astronomy'][0]['moon_phase'];?>)</h3></div>
             </div>
         </div>
         <footer class="container-fluid bg-4 text-center">
